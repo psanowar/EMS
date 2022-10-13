e0f5fcc4a8d3c9cbfd97b3fbd7f548310d3e5863
sanowar-hossain <sanowar2080@gmail.com>
2022-10-13 16:58:24 +0600

    Intregating Admin Template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,32 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+<head>
+
+    @include('layouts.pages.css')
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        @include('layouts.pages.sidebar')
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        @include('layouts.pages.header')
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                @include('layouts.pages.body')
+
+    <!-- Bootstrap core JavaScript-->
+    
+    @include('layouts.pages.script')
+
+</body>
+
+</html>
--- resources/views/layouts/pages/script.blade.php
+++ resources/views/layouts/pages/script.blade.php
@@ -1,4 +1,4 @@
-<script src="admin/vendor/jquery/jquery.min.js"></script>
+    <script src="admin/vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
